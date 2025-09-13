775e1eaf3b92ee50333709a602dd5bb2cc695cc5
Øystein Bringsli <oysteinmb@gmail.com>
2025-09-13 23:00:00 +0200

    Test hashing speed

--- tests/SolvingSpeed.cpp
+++ tests/SolvingSpeed.cpp
@@ -6,7 +6,47 @@
 #include "RubiksLibrary/Lookup.hpp"
 #include "RubiksLibrary/RubiksCube.hpp"
 
-int main() {
+#define MILLION 1000000
+
+void confirmSameResult() {
+	RubiksCube cube;
+
+	int numHashes = 1 * MILLION;
+	for (int i = 0; i < numHashes; i++) {
+		cube.shuffle(50);
+		auto h0 = cube.hashCrossAnd2CornersV1();
+		auto h1 = cube.hashCrossAnd2CornersV2();
+
+		if (h0 != h1) {
+			std::cout << "Invalid hash..?" << "\n";
+			return;
+		}
+	}
+
+	std::cout << "For " << numHashes << " hashes, the functions are equal." << "\n";
+}
+
+void testHashingSpeed() {
+	RubiksCube cube;
+	cube.shuffle(50);
+	int numHashes = 1 * MILLION;
+
+	auto now = std::chrono::high_resolution_clock::now();
+
+	for (int i = 0; i < numHashes; i++) {
+		cube.hashCrossAnd2CornersV2();
+	}
+
+	auto after = std::chrono::high_resolution_clock::now();
+	auto totTime = std::chrono::duration_cast<std::chrono::microseconds>(after - now).count();
+	std::cout << "Total time for " << numHashes << " hashes: " << totTime / 1000 << "ms | Avg time: " << static_cast<double>(totTime) / static_cast<double>(numHashes)
+	<< "us\n";
+
+	// v0: Total time for 1000000 hashes: 2094ms | Avg time: 2.09411us
+	// v1: Total time for 1000000 hashes:  216ms | Avg time: 0.216571us
+}
+
+void testNumSolvingMoves() {
 	Solver solver = Solver();
 	RubiksCube cube = RubiksCube();
 
@@ -15,13 +55,12 @@ int main() {
 
 	unsigned long totNumMoves = 0;
 
-	int num_test = 1000;
+	int num_test = 100;
 	auto now = std::chrono::high_resolution_clock::now();
 	for (int i = 0; i < num_test; i++) {
 		cube.shuffle(50);
 		auto solvingMoves = solver.solveFullCube(cube, lookup);
 		totNumMoves += solvingMoves.size();
-
 
 		auto t1 = std::chrono::high_resolution_clock::now();
 		auto currTime = std::chrono::duration_cast<std::chrono::milliseconds>(t1 - now).count();
@@ -38,4 +77,11 @@ int main() {
 	auto totTime = std::chrono::duration_cast<std::chrono::milliseconds>(after - now).count();
 	std::cout << "Total time: " << totTime << " | Avg time: " << static_cast<double>(totTime) / static_cast<double>(num_test)
 	<< "\n";
+}
+
+int main() {
+	// confirmSameResult();
+	// testHashingSpeed();
+
+	testNumSolvingMoves();
 }
